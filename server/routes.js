74e441135d0775d22ea0a0257688d127a3a90410
sharebaan <sharebaan@gmail.com>
2016-04-09 17:13:07 +0000

    search url done

--- server/routes.js
+++ server/routes.js
@@ -9,26 +9,65 @@ exports.setup = function(app,cors,bodyParser){
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));    
   
-  app.get('/da',function(req, res){
+  var Promise = require("node-promise").Promise;
+  
+  
+  app.get('/search',function(req, res){
     
-    //res.json({pulamea:req.query.search});
     var graph = require('fbgraph');
-    var Promise = require("node-promise").Promise;
-    var promise = new Promise();
+    graph.setAccessToken('CAACEdEose0cBANMxUHc4cSEYRUQ5YD2xvFY4NDDWABZBHfGgy4QBrcTQFxijqEyB0hozxhnaJtZCgArS86f9HmDZCPZBU7wavwFaRwx9LZB9iZAuo5ukIMITKTRJGAZAGJYIVJprXyn9YQ5ZCfxPPD0PcU4CTF1BcQeXZB7VztmOVGNaUZBsh9BYdZBarHT4QzL0wrS9yPejr8t3lidal0ZBwwdS');
     
-    graph.setAccessToken("CAACEdEose0cBAJw2ms3d5qm2IWcZA7WfqydddOn2HI1wAV38ZAZCSgnwuZAId8fZB1HHFkjFpNf7OjaAnDNl3U5fuTiZA7M9dlZCmTq9aR3LdN2iRrUH3XALh7UttpuHZCmtIh0ZAqvEMZBv2fGrlxuZATcT8ZCi6m27NHrp6966M6APwZCjhzLJpen3Kax4WJBXLbDZB7wEIgyWcGRvJzlFnxsbPu");
+    var promiseProfile = new Promise();
+    var promisePicture = new Promise();
 
-    graph.get("search?q="+req.query.search+"&type=page&limit=10", function(err, res){
-        promise.resolve(res);
+    graph.get("search?q="+req.query.find+"&type=page&limit=5", function(err, res){
+        promiseProfile.resolve(res);
     });
-    promise.then(function(response){
+    
+    promiseProfile.then(function(response){
+      //res.json(response);
+        var parent = this;
+        this.count = 0;
+        var a = [];
+        //console.log(response.data);
+        for(var i=0;i<response.data.length;i++){
+              //response.data[i];
+              
+              graph.get(response.data[i].id+'/picture', function(err, resp){  
+                parent.count++;
+                a.push(resp);
+                if (parent.count == 5) {
+                  promisePicture.resolve({profiles: response.data,images: a});
+                }
+              });
+             
+        }  
+    });
+    
+    promisePicture.then(function(profilesImages){
+      //console.log(profilesImages);
+      for(var i = 0; i< profilesImages.profiles.length;i++){
+        var profile = profilesImages.profiles[i];
+        var image = profilesImages.images[i];
+        profile.image ={has: image.image, location: image.location};
+      }
+      res.json(profilesImages.profiles);
+    });
+    
+    /*promisePicture.then(function(r){
+      console.log(r);
+    });*/
+    /*promise.then(function(response){
       res.json(response.data);
       //console.log(response.data);
       /*graph.get(response.data[0].id+'/feed?limit=50',function(err ,result){
         res.json(result.data);
       });*/
-    });
+    //});
+    console.log("Vlad Suge Pula");
    
   });  
+  
+  // test id 164960706892498
   
 };
